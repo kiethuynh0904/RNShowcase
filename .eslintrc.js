a4e3b537a25c48a21848df39e3bf4e388e8568f9
kiet.huynh <kiethuynh0904@gmail.com>
2025-07-24 15:10:37 +0700

    chore: setup ESLint with Prettier, React Native, and TypeScript support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'react-native', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'react-native/no-inline-styles': 'off',
+    'react-native/split-platform-components': 'off',
+  },
 };
--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@
  *
  * @format
  */
-
+import React from 'react';
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 

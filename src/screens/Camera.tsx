0d59be43bef20bd09792e25fd65c8cbe2ca0f99e
kiet.huynh <kiethuynh0904@gmail.com>
2025-07-24 16:52:06 +0700

    feat: setup vision camera

--- src/screens/Camera.tsx
+++ src/screens/Camera.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import {
+  Templates,
+  Camera as VisionCamera,
+  useCameraDevice,
+  useCameraFormat,
+  useCameraPermission,
+} from 'react-native-vision-camera';
 
 export default function Camera() {
+  const device = useCameraDevice('back');
+  const format = useCameraFormat(device, Templates.Snapchat);
+
+  const { hasPermission, requestPermission } = useCameraPermission();
+  if (!hasPermission) {
+    return (
+      <View>
+        <Text>No permission</Text>
+        <Button title="Request permission" onPress={requestPermission} />
+      </View>
+    );
+  }
+  if (device == null) return <Text>No camera found</Text>;
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <VisionCamera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}
+      format={format}
+    />
   );
 }
